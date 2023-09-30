939334436aecc3595737fba90d4cfe3add552773
Lucian Popescu <lucian.popescu187@gmail.com>
2023-09-30 19:56:18 +0100

    Partially fix brl-cad errors for arm

--- pts/brl-cad-1.4.0/install.sh
+++ pts/brl-cad-1.4.0/install.sh
@@ -3,6 +3,13 @@ tar -xf brlcad-7.34.0.tar.bz2
 sed -i \
 	'154s/.*/#if (defined(_M_X64) || defined(_M_ARM64) || defined(_M_IX86) || defined(__i386__) || defined(__x86_64__) || defined(__x86_64) || defined(__aarch64__))/g' \
 	brlcad-7.34.0/src/other/openNURBS/opennurbs_system_runtime.h
+sed -i 's/register//g' brlcad-7.34.0/src/other/openNURBS/opennurbs_evaluate_nurbs.cpp
+grep -rInwl __va_copy brlcad-7.34.0/misc/tools/xmltools/libxml | xargs sed -i 's/__va_copy/va_copy/g'
+sed -i '93d' brlcad-7.34.0/misc/tools/xmltools/libxml/CMakeLists.txt
+sed -i '103i #include <ctype.h>
+#if !defined(isascii) && defined(__isascii)
+#  define isascii __isascii
+#endif' brlcad-7.34.0/src/other/ext/stepcode/src/express/expscan.l
 cp brlcad-7.34.0/src/other/libpng/scripts/pnglibconf.h.prebuilt brlcad-7.34.0/src/other/libpng/pnglibconf.h
 mkdir brlcad-7.34.0/build
 cd brlcad-7.34.0/build
