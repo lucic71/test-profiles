0040fa6da8592c9c0a0e37217f6aa655600b58c6
Lucian Popescu <lucian.popescu187@gmail.com>
2025-03-09 12:14:23 +0200

    Update compilation steps for scimark2
    LTO breaks SparseCompRow, so build it without LTO

--- pts/scimark2-1.3.2/install.sh
+++ pts/scimark2-1.3.2/install.sh
@@ -2,7 +2,20 @@
 
 unzip -o scimark2_1c.zip -d scimark2_files
 cd scimark2_files/
-$CC $CFLAGS -o scimark2 *.c -lm
+$CC -c FFT.c
+$CC -c LU.c
+$CC -c MonteCarlo.c
+$CC -c Random.c
+$CC -c SOR.c
+ORIG_UB_OPT_FLAG=$UB_OPT_FLAG
+UB_OPT_FLAG="$UB_OPT_FLAG -fno-lto"
+$CC -c SparseCompRow.c -fno-lto
+UB_OPT_FLAG="$ORIG_UB_OPT_FLAG"
+$CC -c Stopwatch.c
+$CC -c array.c
+$CC -c kernel.c
+$CC -c scimark2.c
+$CC -o scimark2 scimark2.o FFT.o kernel.o Stopwatch.o Random.o SOR.o SparseCompRow.o array.o MonteCarlo.o LU.o -lm
 echo $? > ~/install-exit-status
 cd ..
 
